13c18c9d30902f81563492d5e84cbabb612a1568
soto97 <soto97@gmail.com>
2017-02-07 16:39:45 -0700

    Created a script to generate a generic proposal folder
    I created a script that generate a generic proposal folder. This folder
    includes sub-directories, the standard sub-directories that I use in
    every proposal.
    
    	new file:   make_proposal_subdirs.sh

---
+++ make_proposal_subdirs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script creates the default sub-directories for a new proposal.
+# These sub-directories include 'Step 1', 'Step 2', 'background_science',
+# and others.
+
+mkdir background_science
+mkdir background_engineering
+mkdir budget
+mkdir planning
+mkdir roses_docs
+mkdir step1
+mkdir step2
+
+
